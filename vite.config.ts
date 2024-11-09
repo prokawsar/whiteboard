8f0a3d0a1c3c9ccf7d7ddc7fd91ac2d77d4b617d
prokawsar <prokawsar@gmail.com>
2024-11-09 13:42:01 +0600

    Fix url

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 const MODE = process.env.NODE_ENV;
+const API_URL =
+	MODE == 'development' ? 'http://localhost:1000' : 'https://whiteboard-axbr.onrender.com/';
+
+console.log(API_URL);
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
-			'/api':
-				MODE == 'development' ? 'http://localhost:1000' : 'https://whiteboard-axbr.onrender.com/'
+			'/api': API_URL
 		}
 	}
 });
