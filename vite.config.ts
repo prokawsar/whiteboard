badf9c1ba675260677c282be948aacdae85e3c38
prokawsar <prokawsar@gmail.com>
2024-11-09 13:32:53 +0600

    Build mode update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const MODE = process.env.NODE_ENV;
+
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
-			'/api': 'http://localhost:1000/'
+			'/api':
+				MODE == 'development' ? 'http://localhost:1000' : 'https://whiteboard-axbr.onrender.com/'
 		}
 	}
 });
