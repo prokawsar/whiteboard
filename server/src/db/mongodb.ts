7c264848e5ee25d8fdff18298245339b91fcad61
prokawsar <prokawsar@gmail.com>
2024-11-11 08:34:31 +0600

    Remove

--- server/src/db/mongodb.ts
+++ server/src/db/mongodb.ts
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL =
-	'mongodb+srv://prokawsar:2o0TKPKMtoqfq26K@main-cluster.uwbda.mongodb.net/?retryWrites=true&w=majority&appName=main-cluster';
 const MONGODB_URL_DEV = 'mongodb://localhost:27017/whiteboards';
+const MONGODB_URL = process.env.MONGODB_URL || '';
 
 export default {
 	connect: async () => {
